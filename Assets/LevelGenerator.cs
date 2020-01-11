3f72dc70f634082e0ee7160ac979752ecbe7de88
MarioV1441 <mariovaquero.sia@gmail.com>
2020-01-11 15:25:47 -0500

    no message

---
+++ Assets/LevelGenerator.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+
+public class LevelGenerator : MonoBehaviour
+{
+    public Texture2D map;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        generateLevel();
+
+    }
+
+    void generateLevel()
+    {
+        for (int x = 0; x < map.width; x++)
+        {
+            for (int y = 0; y < map.height; y++)
+            {
+                GenerateTile(x, y);
+            }
+        }
+    }
+    void GenerateTile(int x, int y)
+    {
+        Color pixelcolor = map.GetPixel(x, y);
+
+        if (pixelcolor.a == 0)
+        {
+            //ignores transparent pixels
+            return;
+        }
+
+        Debug.Log("Pixel Color");
+    }
+}
---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    public Vector3 up;
+    public Vector3 down;
+    public Vector3 left;
+    public Vector3 right;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            GetComponent<Transform>().position + up;
+        }
+
+
+    }
+
+ }
+
