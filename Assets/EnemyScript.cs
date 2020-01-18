46d9bc69f59870b0bdef795f8d95d6f91d8edc6e
MarioV1441 <mariovaquero.sia@gmail.com>
2020-01-18 11:29:10 -0500

    Test Commit

---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class EnemyScript : MonoBehaviour
+{
+    public GameObject Player;
+    public float movementSpeed = 4;
+
+    private void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        transform.LookAt(Player.transform);
+        transform.position += transform.forward * movementSpeed * Time.deltaTime;
+    }
+}
