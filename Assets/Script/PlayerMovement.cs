45febad69daa21515b970e3c5a53d98869d83a32
MarioV1441 <mariovaquero.sia@gmail.com>
2020-02-01 11:30:20 -0500

    Added better Player Movement script in order to not have problems with physics

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float MoveSpeed = 10f;
+
+    public Rigidbody2D RB;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // This Is The Input For The Movement
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+    private void FixedUpdate()
+    {
+
+        //This Is The Movement
+
+        RB.MovePosition(RB.position + movement * MoveSpeed * Time.fixedDeltaTime);
+        
+    }
+}
