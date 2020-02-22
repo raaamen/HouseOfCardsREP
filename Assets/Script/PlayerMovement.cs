e03c9035ba7bd33e73ffb7d114b8573c6e36ede2
Kymani <>
2020-02-22 12:46:14 -0500

    2/22/20. *Made player flip and player attack flip when facing left

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float MoveSpeed = 10f;
+    //public float speed;
+    private float moveX;
+    private float moveY;
+
+    private bool facingRight = true;
+    private bool facingUp = true;
 
     public Rigidbody2D RB;
+    //private Rigidbody2D rb;
 
     Vector2 movement;
+
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody2D>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-
         // This Is The Input For The Movement
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
@@ -24,7 +35,44 @@ public class PlayerMovement : MonoBehaviour
         //This Is The Movement
 
         RB.MovePosition(RB.position + movement * MoveSpeed * Time.fixedDeltaTime);
-        
+
+        /*
+        moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+        Debug.Log(moveX + moveY);
+        rb.velocity = new Vector3(moveX, moveY * speed, rb.velocity.y);
+        */       
+
+        if (facingRight == false && movement.x > 0)
+        {
+            FlipX();
+        }
+        else if (facingRight == true && movement.x < 0)
+        {
+            FlipX();
+        }
+        if (facingUp == false && movement.y > 0)
+        {
+            FlipY();
+        }
+        else if (facingUp == true && movement.y < 0)
+        {
+            FlipY();
+        }
+    }
+    void FlipX()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+    void FlipY()
+    {
+        facingUp = !facingUp;
+        Vector3 Scaler = transform.localScale;
+        Scaler.y *= -1;
+        transform.localScale = Scaler;
     }
 
 }
