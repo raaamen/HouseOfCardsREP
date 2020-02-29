384916cd9b269ca1c855b42eda7d81db918a2516
Kymani <>
2020-02-29 14:18:48 -0500

    no message

---
+++ Assets/Script/NumberFourCard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberFourCard : MonoBehaviour
+{
+    public int HP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HP = 4;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/NumberTwoCard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberTwoCard : MonoBehaviour
+{
+    public int HP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HP = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
