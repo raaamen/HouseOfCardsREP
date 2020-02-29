00ad9e9b3e5cf6d40f74e06b436d6f931a20c144
MarioV1441 <mariovaquero.sia@gmail.com>
2020-02-29 16:40:37 -0500

    DialogueTrigger

--- Assets/DialogueTrigger.cs
+++ Assets/DialogueTrigger.cs
@@ -22,13 +22,11 @@ public class DialogueTrigger : MonoBehaviour
             FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
         
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            playerHere = true;
-            Debug.Log("beep");
+
         }
     }
-
 }
