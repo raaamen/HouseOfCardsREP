e7178de46e55aeaf8631f8d14eb7da8bbfc02523
MarioV1441 <mariovaquero.sia@gmail.com>
2020-05-30 13:20:40 -0400

    Enemies and sound stuff

---
+++ Assets/Script/AudioManagerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.XR;
+
+public class AudioManagerScript : MonoBehaviour
+{
+    public EnemyScript enemyScript;
+    public AudioSource ESD;
+    public bool canPlay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyScript = GameObject.FindGameObjectWithTag("SwordEnemy").GetComponent<EnemyScript>();
+        canPlay = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemyScript.curHealth <=0 && canPlay == true )
+        {
+            GetComponent<AudioSource>().Play();
+            canPlay = false;
+        }
+    }
+}
