cbdd11a391bf34aec2dbf6eb6442ff96c066f18f
MarioV1441 <mariovaquero.sia@gmail.com>
2020-06-06 16:33:12 -0400

    Created the new enemy and started work on his attacks

--- Assets/EnemyHScript.cs
+++ Assets/EnemyHScript.cs
@@ -1,9 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
-public class EnemyScript : MonoBehaviour
+public class EnemyHScript : MonoBehaviour
 {
     //Refrences
     private Transform Target;
@@ -18,28 +17,26 @@ public class EnemyScript : MonoBehaviour
     public int curHealth;
     public int maxHealth;
 
-    //Animations
-    public Animator anim;
+    public Animator jump;
 
-
-    private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        //targets any gameobject with the tag "Player"
         Target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-        //gets the PlayerScript from any gameobject with the tag "Player"
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerScript>();
 
         movementSpeed = 2;
 
         maxHealth = 5;
 
-
         curHealth = maxHealth;
     }
 
+    // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards( transform.position, Target.position , movementSpeed*Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, Target.position, movementSpeed * Time.deltaTime);
+
 
         if (dazedTime <= 0)
         {
@@ -63,18 +60,15 @@ public class EnemyScript : MonoBehaviour
         {
             player.Damage(5);
         }
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Range"))
         {
-            anim.SetBool("inRange", true );
+            jump.SetBool("jump", true);
+        }else
+        {
+            jump.SetBool("jump", false);
         }
     }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            anim.SetBool("inRange", false);
-        }
-    }
+    
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Bullet"))
