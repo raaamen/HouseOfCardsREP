c9c3716ae2b40d49869605daa51e5d6b043a8862
sarapppng <sarahapplebaum.sia@gmail.com>
2020-06-13 13:16:10 -0400

    *Added transition from cinematic scene

---
+++ Assets/LevelChanger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelChanger : MonoBehaviour
+{
+
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FadeToLevel(string levelName)
+    {
+        animator.SetTrigger("FadeOut");
+    }
+
+}
--- Assets/Script/CinematicOpScript.cs
+++ Assets/Script/CinematicOpScript.cs
@@ -7,16 +7,22 @@ using UnityEngine.SceneManagement;
 public class CinematicOpScript : MonoBehaviour
 {
 
-    public List<Image> images;
+    public List<Sprite> images;
 
     public Image imageHolder;
+    public Image blackScreen;
+
+    public LevelChanger level;
+
+    public bool fading;
 
     public int i;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        i = 0;
+        imageHolder.sprite = images[i];
     }
 
     // Update is called once per frame
@@ -26,8 +32,26 @@ public class CinematicOpScript : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                i++;
+                level.animator.SetTrigger("FadeOut");
+                StartCoroutine("wait");
+                Debug.Log(i);
+
             }
         }
+
+        else
+        {
+            SceneManager.LoadScene("Basement");
+        }
     }
+
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(1f);
+        i++;
+        imageHolder.sprite = images[i];
+    }
+
+    
+
 }
